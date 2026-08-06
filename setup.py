36c600a9d9e290726cd71f1ed8235229c86b6c35
Masudur Rahman <masudaucb1303@gmail.com>
2026-08-07 02:58:33 +0600

    Project and env set up completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Customer_Churn_Prediction_MLOps",
+    version = "0.0.1",
+    author = "Masudur Rahman",
+    author_email = "masudaucb1303@gmail.com",
+    packages = find_packages()
+)
